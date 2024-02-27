a623c5e0c67f77d151217f3089c6a0ea8fc8b57c
Noshinc346 <noshin.chowdhury31@gmail.com>
2024-02-27 09:15:48 -0500

    2/27 13-mumble

--- index.js
+++ index.js
@@ -22,3 +22,27 @@
           return count;
         }
       }
+
+// 2/27 Challenge: 13-mumble
+    // Prompt:
+    // - Write a function called mumble that accepts a single string argument.
+    // - The function should return a string that has each character repeated the number of times according to its position within the string arg.  In addition, each repeated section of characters should be separated by a hyphen (-).
+    // - Examples describe it best..
+    // Examples:
+    // mumble('X'); //=> 'X'
+    // mumble('abc'); //=> 'a-bb-ccc'
+    // mumble('121'); //=> '1-22-111'
+    // mumble('!A 2'); //=> '!-AA-   -2222'
+
+
+    function mumble(str) {
+        let arrStr = str.split('')
+        let newArr = []
+        for (let i = 0; i < arrStr.length; i++) {
+          newArr.push(arrStr[i].repeat(i+1))
+        }
+          return newArr.join('-')
+      }
+      
+      console.log(mumble("abc"))
+      console.log(mumble("!A 2"))
