b99dc4612c8387b8471875c3466b2f2822df8b51
Noshinc346 <noshin.chowdhury31@gmail.com>
2024-02-29 09:13:49 -0500

    2/29 Daily Challenge complete

--- Daily-Challenges/index.js
+++ Daily-Challenges/index.js
@@ -46,3 +46,12 @@
       
       console.log(mumble("abc"))
       console.log(mumble("!A 2"))
+
+// Prompt:
+// - Write a function named fromPairs that creates an object from an array containing nested arrays.
+// - Each nested array will have two elements representing key/value pairs used to create key/value pairs in an object to be returned by the function.
+// - If a key appears in multiple pairs, the rightmost pair should overwrite previous the previous entry in the object.
+// Examples:
+// fromPairs([ ['a', 1], ['b', 2], ['c', 3] ]) //=> { a: 1, b: 2, c: 3 }
+// fromPairs([ ['name', 'Sam"], ['age', 24], ['name', 'Sally'] ]) //=> { name: "Sally", age: 24 }
+
