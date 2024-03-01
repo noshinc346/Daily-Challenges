ed1ed0e84082b54454edc0e1202e1521481436b1
Noshinc346 <noshin.chowdhury31@gmail.com>
2024-03-01 09:34:37 -0500

    3/1 challenge done

--- Daily-Challenges/index.js
+++ Daily-Challenges/index.js
@@ -47,6 +47,7 @@
       console.log(mumble("abc"))
       console.log(mumble("!A 2"))
 
+// 2/29 Challenge 
 // Prompt:
 // - Write a function named fromPairs that creates an object from an array containing nested arrays.
 // - Each nested array will have two elements representing key/value pairs used to create key/value pairs in an object to be returned by the function.
@@ -61,4 +62,22 @@
         obj[nestArr[0]] = nestArr[1];
       })
       return obj
+    }
+
+// 3/1 Challenge 
+// Write a function named mergeObjects that accepts at least two objects as arguments, merges the properties of the second through n objects into the first object, then finally returns the first object.
+// - If any objects have the same property key, values from the object(s) later in the arguments list should overwrite earlier values.
+// Examples:
+// mergeObjects({}, {a: 1});  //=> {a: 1} (same object as first arg)
+// mergeObjects({a: 1, b: 2, c: 3}, {d: 4});  //=> {a: 1, b: 2, c: 3, d: 4}
+// mergeObjects({a: 1, b: 2, c: 3}, {d: 4}, {b: 22, d: 44});  //=> {a: 1, b: 22, c: 3, d: 44} 
+
+    function mergeObjects(target, ...objects) {
+      objects.forEach(function(obj) {
+        // using ES2015's 'for in' loop
+        for(let key in obj) {
+          target[key] = obj[key];
+        }
+      });
+      return target;
     }
