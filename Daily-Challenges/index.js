563da7e3dfeec32ce52198920562e647d3cbe05f
Noshinc346 <noshin.chowdhury31@gmail.com>
2024-03-04 09:41:43 -0500

    challenge not working

--- Daily-Challenges/index.js
+++ Daily-Challenges/index.js
@@ -81,3 +81,32 @@
       });
       return target;
     }
+
+
+// 3/4 Challenge 
+  // Write a function named findHighestPriced that accepts a single array of objects.
+  // - The objects contained in the array are guaranteed to have a price property holding a numeric value.
+  // - The function should return the object in the array that has the largest value held in the price property.
+  // - If there's a tie between two or more objects, return the first of those objects in the array.
+  // - Return the original object, not a copy.
+  // - DO NOT mutate the array, i.e., do not sort it
+  // Examples:
+  // findHighestPriced([
+  //   { sku: 'a1', price: 25 },
+  //   { sku: 'b2', price: 5 },
+  //   { sku: 'c3', price: 50 },
+  //   { sku: 'd4', price: 10 }
+  // ]);
+
+    //Not working
+    function findHighestPriced(items) {
+      let highNumber = 0;
+      let answerObj;
+      items.forEach(function (item) {
+        if (items.price > highNumber) {
+          items.price = highNumber;
+          answerObj = item;
+        }
+        return answerObj;
+      });
+    }
