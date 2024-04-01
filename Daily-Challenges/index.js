605c6d9411010f19460a8116b81220a167464b2d
Noshinc346 <noshin.chowdhury31@gmail.com>
2024-04-01 09:41:33 -0400

    daily challenge 4/1 done

--- Daily-Challenges/index.js
+++ Daily-Challenges/index.js
@@ -146,4 +146,27 @@
   // reduceArray( ['Yes', 'No', 'Yes', 'Maybe'], function(acc, v) {acc[v] = acc[v] ? acc[v] + 1 : 1; return acc;}, {} );
   //=> {"Yes": 2, "No": 1, "Maybe": 1}
 
-  
+  // 4/01 
+    // Difficulty: Intermediate
+    // Prompt:
+    // - Write a function named intersection that accepts two arguments which are both arrays.  The array arguments may contain any mixture of strings, numbers and/or booleans - but no reference types, i.e., objects.
+    // - The function should return a new array containing all elements in common, including repeating element values.
+    // - The ordering of the elements in the returned array is not important.
+    // - If there are no elements in the arrays in common,  the intersection function should return an empty array.
+    // - The function should not mutate (change) either argument.
+    // Examples:
+    // intersection(['a', 1], []) //=> []
+    // intersection(['a', 1], [true, 'a', 15]) //=> ['a']
+    // intersection([1, 'a', true, 1, 1], [true, 1, 'b', 1]) //=> [1, true, 1]
+
+    function intersection(arr1, arr2) {
+      common = [];
+      for(let i = 0; i < arr1.length; i++) {
+        for(let j = 0; j < arr2.length; j++) {
+          if(arr1[i] === arr2[j]) {
+            common.push(arr1[i])
+          }
+        }
+        return common
+      }
+    }
