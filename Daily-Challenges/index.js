d60a9bb9dfd3c269fed743375b2e246dee19b427
Noshinc346 <noshin.chowdhury31@gmail.com>
2024-04-02 09:32:13 -0400

    4/2 challenge

--- Daily-Challenges/index.js
+++ Daily-Challenges/index.js
@@ -170,3 +170,37 @@
         return common
       }
     }
+
+// 4/02 
+    // Difficulty:  Intermediate
+    // Prompt:
+    // - Write a function called balancedBrackets that accepts a single string as argument.
+    // - The input string is composed entirely of parentheses, brackets and/or curly braces, i.e.,  (), [] and/or {}. Referred to as "braces" from this point forward...
+    // - The balancedBraces function should return true if the string's braces are "balanced" and false if they are not.
+    // - The brackets are considered unbalanced if any closing bracket does not close the same type of opening bracket, ignoring already matched brackets between them.  Examples explain it best...
+    // Examples:
+    // balancedBrackets( '()' ) // => true
+    // balancedBrackets( '(]' ) // => false
+    // balancedBrackets( '[{}]' ) // => true
+    // balancedBrackets( '[(])' ) // => false
+    // balancedBrackets( '[({}[])]' ) // => true
+
+    function balancedBrackets(str) {
+      const brackets = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+      }
+      const stack = []
+      for (let i = 0; i < str.length; i++) {
+        if (brackets[str[i]]) {
+          stack.push(str[i])
+        } else {
+          const last = stack.pop()
+          if (str[i] !== brackets[last]) {
+            return false
+          }
+        }
+      }
+      return stack.length === 0
+    }
