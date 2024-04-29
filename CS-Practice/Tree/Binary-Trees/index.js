9f6f0121542dc3c1e1c782a4be579a74cd114f75
Noshinc346 <noshin.chowdhury31@gmail.com>
2024-04-29 12:57:50 -0400

    New binary Question. Leetcode 543 Diameter of Binary Tree

--- CS-Practice/Tree/Binary-Trees/index.js
+++ CS-Practice/Tree/Binary-Trees/index.js
@@ -48,3 +48,23 @@ class binarySearchTree {
 
 // const tree = new binarySearchTree()
 // console.log(tree)
+
+
+//Leetcode 543: Diameter of Binary Tree 
+
+const diameterOfBinaryTree = function(root) {
+    const diameter = (node,res) => {
+        if (!node) return 0;
+        const left = diameter(node.left,res);
+        const right = diameter(node.right,res);
+
+        res[0] = Math.max(res[0], left + right);
+
+        return Math.max(left, right) + 1;
+
+    }
+
+    const res = [0]
+    diameter(root, res);
+    return res[0];
+};
