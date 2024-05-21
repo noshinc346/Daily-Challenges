820b677ff841e50a548edb0985909760e70792f0
Noshinc346 <noshin.chowdhury31@gmail.com>
2024-05-21 13:08:46 -0400

    leetcode 1 twosum

--- CS-Practice/Hashtable/index.js
+++ CS-Practice/Hashtable/index.js
@@ -34,3 +34,21 @@ var isAnagram = function(s, t) {
 
     return true;
 }
+
+//LeetCode 1. Two Sum
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+
+var twoSum = function(nums, target) {
+    const pairIdx = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        if (target - num in pairIdx) {
+            return [i, pairIdx[target - num]];
+        }
+        pairIdx[num] = i;
+    }    
+};
