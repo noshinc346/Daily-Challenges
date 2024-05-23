dfce1e557f6c19b1ceb25ecae7eb7635a3c86d58
Noshinc346 <noshin.chowdhury31@gmail.com>
2024-05-23 17:44:09 -0400

    leetcode 21 merge linked lists sorted

--- CS-Practice/Linked-Lists/index.js
+++ CS-Practice/Linked-Lists/index.js
@@ -330,3 +330,35 @@ var reverseList = function(head) {
   }
   return prev;
 };
+
+
+// Leetcode 21. Merge Two Sorted Lists
+// You are given the heads of two sorted linked lists list1 and list2.
+// Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+// Return the head of the merged linked list.
+
+
+const dummy = new ListNode();
+let current = dummy;
+
+// Traverse both lists until one of them is exhausted
+while (list1 !== null && list2 !== null) {
+    if (list1.val < list2.val) {
+        current.next = list1;  // Attach list1 node
+        list1 = list1.next;    // Move to the next node in list1
+    } else {
+        current.next = list2;  // Attach list2 node
+        list2 = list2.next;    // Move to the next node in list2
+    }
+    current = current.next;    // Move to the next node in the merged list
+}
+
+// Attach the remaining nodes of list1 or list2
+if (list1 !== null) {
+    current.next = list1;
+} else {
+    current.next = list2;
+}
+
+// Return the head of the merged list (next node of dummy)
+return dummy.next;
