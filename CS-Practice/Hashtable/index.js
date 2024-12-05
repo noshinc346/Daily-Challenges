ddcd3a7a1560c0b90b998f51303dd6a65dcd04a1
Noshinc346 <noshin.chowdhury31@gmail.com>
2024-12-05 15:27:29 -0500

    leetcode 219

--- CS-Practice/Hashtable/index.js
+++ CS-Practice/Hashtable/index.js
@@ -53,4 +53,25 @@ var twoSum = function(nums, target) {
     }    
 };
 
-// Review 
+// Leetcode 219 : Contains Dup II
+
+const containsNearbyDuplicate = (nums, k) => {
+
+    const index_map = new Map();
+  
+    for (let i = 0; i < nums.length; i++) {
+  
+      // If the number already exists in the map, check the difference
+      if (index_map.has(nums[i])) {
+        // should use Math.abs() to ensure the difference is non-negative.
+        let diff = Math.abs(index_map.get(nums[i]) - i);
+        if (diff <= k) {
+          return true
+        }
+      }
+      // Add it to the map with the current index of the number
+      index_map.set(nums[i], i);
+    }
+  
+    return false;
+  };
