ac2050795459f0cc4e928120de4807a099d74968
Noshinc346 <noshin.chowdhury31@gmail.com>
2024-12-05 18:13:09 -0500

    leetcode 53. Maximum Subarray

--- CS-Practice/Array/index.js
+++ CS-Practice/Array/index.js
@@ -169,3 +169,21 @@ var productExceptSelf = function(nums) {
 
     return output;    
 };
+
+//Leetcode 53. 
+
+var maxSubArray = function(nums) {
+    let res = nums[0];
+    let total = 0;
+
+    for (let n of nums) {
+        if (total < 0) {
+            total = 0;
+        }
+
+        total += n;
+        res = Math.max(res, total);
+    }
+    
+    return res;    
+};
